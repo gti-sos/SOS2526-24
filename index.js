94317ef5f266b3a8d26e85ea3fc3438e91f79a90
mariaajesuus <marjimpal@alum.us.es>
2026-03-09 13:24:00 +0100

    codigo

--- index.js
+++ index.js
@@ -20,30 +20,16 @@ const port = process.env.PORT || 3000;
 
 //Datos
 let datosMaria = [];
-
-let initialDatosMaria = [
-    { year: 2024, country: "canada", avg_monthly_nc: 6926.7, avg_monthly_usd: 5056.8, currency: "CAD" },
-    { year: 2023, country: "canada", avg_monthly_nc: 6750.4, avg_monthly_usd: 5001.8, currency: "CAD" },
-    { year: 2022, country: "canada", avg_monthly_nc: 6513.1, avg_monthly_usd: 5004.2, currency: "CAD" },
-    { year: 2023, country: "usa", avg_monthly_nc: 6678.3, avg_monthly_usd: 6678.3, currency: "USD" },
-    { year: 2024, country: "uk", avg_monthly_nc: 3733.8, avg_monthly_usd: 4771.6, currency: "GBP" },
-    { year: 2022, country: "spain", avg_monthly_nc: 2531.6, avg_monthly_usd: 2662.2, currency: "EUR" },
-    { year: 2023, country: "ireland", avg_monthly_nc: 4496.2, avg_monthly_usd: 4862.3, currency: "EUR" },
-    { year: 2023, country: "poland", avg_monthly_nc: 6873.8, avg_monthly_usd: 1636.4, currency: "PLN" },
-    { year: 2023, country: "italy", avg_monthly_nc: 2704.2, avg_monthly_usd: 2924.4, currency: "EUR" },
-    { year: 2022, country: "germany", avg_monthly_nc: 3790.4, avg_monthly_usd: 3986.0, currency: "EUR" }
-];
-
+const initialDatosMaria = mjp.datosMaria;
 
 let datosIrg=[]
 const datosIsaac = irg.datosIsaac; 
-
 
 let datosEBP=[]
 const initialDatosEBP = ebp.datos; 
 
 
-
+//Rutas simples (F04)
 app.use("/", express.static("./public")); 
 app.use(bodyParser.json());
 
@@ -142,7 +128,7 @@ app.get(MJP_API_PATH + "/loadInitialData", (req, res) => {
         datosMaria = [...initialDatosMaria];
         res.status(201).json(datosMaria); // 201 Created
     } else {
-        res.status(400).json({ error: "El array ya contiene datos." }); // 400 Bad Request
+        res.status(409).json({ error: "El array ya contiene datos." }); // 409 Conflict
     }
 });
 
