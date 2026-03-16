e7ede3e1217a7596d51b1630237a52b3c47bd07d
elenabejper <elebejper@alum.us.es>
2026-03-16 09:33:56 +0100

    index.html

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ loadBackendIsaac(app);
 loadBackendMaria(app);
 loadBackendElena(app);
 
+/*
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
-
+*/
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
