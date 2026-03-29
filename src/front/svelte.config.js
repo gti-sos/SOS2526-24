9dd9f8ea2305d187b870ff5031c15840cac497e9
IsaacRg25 <isaac@isaac.localdomain>
2026-03-29 21:16:48 +0200

    instalacion tests prueba2

--- src/front/svelte.config.js
+++ src/front/svelte.config.js
@@ -2,11 +2,17 @@ import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
-	vitePlugin: {
-		dynamicCompileOptions: ({ filename }) =>
-			filename.includes('node_modules') ? undefined : { runes: true }
-	}
+    kit: { 
+        adapter: adapter({
+            // Esto asegura que se cree la carpeta 'build' dentro de 'src/front'
+            out: 'build' 
+        }) 
+    ,
+    vitePlugin: {
+        dynamicCompileOptions: ({ filename }) =>
+            filename.includes('node_modules') ? undefined : { runes: true }
+    }
+}
 };
 
 export default config;
