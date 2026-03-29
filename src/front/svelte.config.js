cce9d3e735f53d71058194d2d4b7074954ee2d93
IsaacRg25 <isaac@isaac.localdomain>
2026-03-29 21:19:35 +0200

    instalacion tests prueba3

--- src/front/svelte.config.js
+++ src/front/svelte.config.js
@@ -2,17 +2,18 @@ import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: { 
+    kit: {
+        // El adaptador va dentro de kit
         adapter: adapter({
-            // Esto asegura que se cree la carpeta 'build' dentro de 'src/front'
-            out: 'build' 
-        }) 
-    ,
+            out: 'build'
+        })
+    }, // <--- ESTA LLAVE CIERRA EL KIT. Es la que faltaba.
+
+    // vitePlugin va fuera de kit, al mismo nivel
     vitePlugin: {
         dynamicCompileOptions: ({ filename }) =>
             filename.includes('node_modules') ? undefined : { runes: true }
     }
-}
 };
 
 export default config;
